3c727795d3bc49e6109805f47f2e5eef21d57753
Datta_TECH <datta.naik@inventrom.com>
2017-03-10 17:44:06 +0530

    adding comments for documentng ifconfig.me client

--- test1_ifconfig.go
+++ test1_ifconfig.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "strings"
+    "fmt" // for printing
+    "net/http" // for making requests
+    "io/ioutil" // for reading response
+    "strings" // for string manipulation
 )
 
 func main() {
-    recurring_call()
+    recurring_call() // keep tryin till we get data from ifconfig.me
 }
 
 func recurring_call(){
     resp, err := http.Get("http://ifconfig.me/")
-    if (err != nil) {
+    if (err != nil) { // if error in connection
             fmt.Println(err)
             fmt.Println("retrying request to http://ifconfig.me/")
             recurring_call()
     } else {
         defer resp.Body.Close();
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
+        body, err := ioutil.ReadAll(resp.Body) // read the body the output will be an arry of bytes
+        if err != nil { // check for any errors if present in Body
             fmt.Println(err)
             return
         }
-
-        s := string(body[:len(body)])
-        indexOfIPRead := strings.Index(s, "\"ip_addr\":\"")
-        afterIndexOfIPReadString := string(s[indexOfIPRead:len(s)])
-        indexOfComma := strings.Index(afterIndexOfIPReadString, ",")
-        currentIP := string(afterIndexOfIPReadString[10:indexOfComma])
-        fmt.Println("Your Current IP on the Internet is:",currentIP)
+        s := string(body[:len(body)]) // convert byte array to string
+        indexOfIPRead := strings.Index(s, "\"ip_addr\":\"") // get index of "ip_addr": 
+        afterIndexOfIPReadString := string(s[indexOfIPRead:len(s)]) // truncate string
+        indexOfComma := strings.Index(afterIndexOfIPReadString, ",") // find position of comma IP comes in between ip_addr and ,
+        currentIP := string(afterIndexOfIPReadString[10:indexOfComma]) // get current IP
+        fmt.Println("Your Current IP on the Internet is:",currentIP) // print IP
     }
+
 }
 
